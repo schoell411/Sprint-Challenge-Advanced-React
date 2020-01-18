04e8bc1b34c74391eca3f82b8f0ebb477b088645
Kevin Schoell <schoell411@gmail.com>
2020-01-17 18:45:49 -0600

    axios returning data from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  axiosFetch = () => {
+    axios
+    .get(`http://localhost:5000/api/players`)
+    .then(res =>{
+      console.log(res);
+      console.log(res.data[0].name);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
+  componentDidMount() {
+    this.axiosFetch();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Women's World Cup Players </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
