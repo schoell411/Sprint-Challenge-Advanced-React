cd3ef4bfd8fd7e54e7786c030c5241772dd14f71
Kevin Schoell <schoell411@gmail.com>
2020-01-18 00:52:50 -0600

    tests pass

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders without crashing', () => {
+render(<App />);
 });
+
+test('Get h1 title', () => {
+  const { getByText } = render(<App />);
+  getByText(/Women's World Cup Players/i);
+  });
